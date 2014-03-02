1d73a396a806df3e94c6b1519cab03a7d5b6d8f8
YongHun Byun <yonghunbyun@gmail.com>
2014-03-03 00:35:10 +0900

    changed the environment detect way

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,10 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('your-machine-name'),
-
-));
+$env = $app->detectEnvironment(function() {
+    // Default to local if LARAVEL_ENV is not set
+    return getenv('LARAVEL_ENV') ?: 'local';
+});
 
 /*
 |--------------------------------------------------------------------------
