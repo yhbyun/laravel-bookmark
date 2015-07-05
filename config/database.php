ef7e170fc1a4531c9ae22b27a1788e44e0aabc13
YongHun Byun <yonghunbyun@gmail.com>
2015-07-06 01:35:28 +0900

    Change default mysql connection settings

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'database'  => env('DB_DATABASE', 'homestead'),
+			'username'  => env('DB_USERNAME', 'homestead'),
+			'password'  => env('DB_PASSWORD', 'secret'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
