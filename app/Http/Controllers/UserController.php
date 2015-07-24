6fb6bcb78825e5a268b306b66531b4930e8476b0
YongHun Byun <yonghunbyun@gmail.com>
2015-07-24 09:06:01 +0900

    Fix for #6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Exception\HttpResponseException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
