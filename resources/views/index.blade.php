cb5219daece16975b4ab5b935c28d6bf80c7f6ec
Laurent Goussard <loranger@free.fr>
2016-03-10 09:42:50 +0100

    [Upg] Use google analytics if exists in .env file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,15 +51,18 @@
   	<i class="icon-facebook icon-3x icon-light"></i>
 	</span></a>
 </div>
-<script>
+
+@if( env('ANALYTICS_ID', false) )
+	<script>
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	ga('create', 'UA-42064646-1', 'rivario.com');
+	ga('create', '{{ env('ANALYTICS_ID') }}', '{{ \Request::server('HTTP_HOST') }}');
 	ga('send', 'pageview');
 
-</script>
+	</script>
+@endif
 </body>
 </html>
